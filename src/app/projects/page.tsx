ad87cf414e01d4dfe2a9a6c203589f0ad22b28e7
RCOM363 <rahullankeppanavar363@gmail.com>
2025-04-18 13:01:14 +0000

    feat: add blur effect to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,66 +2,80 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { BlurFade } from "@/components/magicui/blur-fade";
 import { projects } from "@/data/projects";
 import { Button } from "@/components/ui/button";
 import { GithubIcon } from "@/components/ui/github";
 import { LinkIcon } from "@/components/ui/link";
 
+const BLUR_FADE_DELAY = 0.04;
+
 function Projects() {
   return (
-    <div className="w-full px-4">
-      <h1 className="text-lg font-bold">Stuff that I&apos;ve worked on</h1>
+    <div className="w-full px-4 pb-5">
+      <BlurFade delay={BLUR_FADE_DELAY * 2}>
+        <h1 className="text-lg font-bold">Stuff that I&apos;ve worked on</h1>
+      </BlurFade>
       <div className="grid grid-cols-1 items-center gap-4 lg:p-4 mt-4">
         {projects.map((project) => (
-          <div
-            key={project.name}
-            className="w-full flex flex-col items-center mx-auto border-2 rounded-lg"
-          >
-            {/* image */}
-            <Link href={project.source} className="overflow-hidden">
-              <Image
-                src={project.image}
-                alt={project.name}
-                width={700}
-                className="transition duration-300 ease-in-out hover:scale-110 rounded-t-lg "
-              />
-            </Link>
-            <div className="flex flex-col justify-start gap-4 p-4">
-              {/* title & description */}
-              <div>
-                <h3 className="text-xl font-semibold">{project.name}</h3>
-                <p className="text-sm text-stone-400">{project.description}</p>
-              </div>
-              {/* technologies */}
-              <div className="w-full flex flex-wrap items-center gap-2">
-                {project.technologies.map((t) => (
-                  <span
-                    key={t}
-                    className="inline-flex items-center text-[12px] px-2.5 py-1 border-none bg-accent rounded-lg"
-                  >
-                    {t}
-                  </span>
-                ))}
-              </div>
-              {/* links */}
-              <div className="flex items-center gap-2">
-                <Link href={project.source} target="_blank">
-                  <Button variant={"default"}>
-                    <GithubIcon />
-                    Source
-                  </Button>
-                </Link>
-                {project.link !== "" && (
-                  <Link href={project.link} target="_blank">
-                    <Button variant={"default"}>
-                      <LinkIcon />
-                      Visit
-                    </Button>
-                  </Link>
-                )}
+          <BlurFade delay={BLUR_FADE_DELAY * 3} key={project.name} inView>
+            <div className="w-full flex flex-col items-center mx-auto border-2 rounded-lg">
+              {/* image */}
+              <Link href={project.source} className="overflow-hidden">
+                <BlurFade delay={BLUR_FADE_DELAY * 3 + 0.05}>
+                  <Image
+                    src={project.image}
+                    alt={project.name}
+                    width={700}
+                    className="transition duration-300 ease-in-out hover:scale-110 rounded-t-lg "
+                  />
+                </BlurFade>
+              </Link>
+              <div className="flex flex-col justify-start gap-4 p-4">
+                {/* title & description */}
+                <BlurFade delay={BLUR_FADE_DELAY * 3 + 0.06}>
+                  <div>
+                    <h3 className="text-xl font-semibold">{project.name}</h3>
+                    <p className="text-sm text-stone-400">
+                      {project.description}
+                    </p>
+                  </div>
+                </BlurFade>
+                {/* technologies */}
+                <BlurFade delay={BLUR_FADE_DELAY * 3 + 0.07}>
+                  <div className="w-full flex flex-wrap items-center gap-2">
+                    {project.technologies.map((t) => (
+                      <span
+                        key={t}
+                        className="inline-flex items-center text-[12px] px-2.5 py-1 border-none bg-accent rounded-lg"
+                      >
+                        {t}
+                      </span>
+                    ))}
+                  </div>
+                </BlurFade>
+                {/* links */}
+                <BlurFade delay={BLUR_FADE_DELAY * 3 + 0.08}>
+                  <div className="flex items-center gap-2">
+                    <Link href={project.source} target="_blank">
+                      <Button variant={"default"}>
+                        <GithubIcon />
+                        Source
+                      </Button>
+                    </Link>
+                    {project.link !== "" && (
+                      <Link href={project.link} target="_blank">
+                        <Button variant={"default"}>
+                          <LinkIcon />
+                          Visit
+                        </Button>
+                      </Link>
+                    )}
+                  </div>
+                </BlurFade>
               </div>
             </div>
-          </div>
+          </BlurFade>
         ))}
       </div>
     </div>
