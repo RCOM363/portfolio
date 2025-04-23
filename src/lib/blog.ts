829a6d5bcba205e68a18c44e40eafb5eb7c28b1d
RCOM363 <rahullankeppanavar363@gmail.com>
2025-04-23 11:02:53 +0000

    refactor: add cache to blog.ts

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,27 +1,26 @@
 import fs from "fs/promises";
 import path from "path";
 import matter from "gray-matter";
+import { cache } from "react";
 
 import { Blog } from "@/types/blog.types";
 
 const blogsDirectory = path.join(process.cwd(), "src/data/blogs");
 
 // get all blogs
-export async function getBlogs(): Promise<Blog[]> {
+export const getBlogs = cache(async (): Promise<Blog[]> => {
   const fileNames = await fs.readdir(blogsDirectory);
+
   const allBlogsData = await Promise.all(
     fileNames
-      // get files with .mdx only
       .filter((fileName) => fileName.endsWith(".mdx"))
       .map(async (fileName) => {
-        // extract slug i.e filename
         const slug = fileName.replace(/\.mdx$/, "");
-        // get file content
         const fullPath = path.join(blogsDirectory, fileName);
         const fileContents = await fs.readFile(fullPath, "utf8");
 
         const { data, content } = matter(fileContents);
-        // return as Blog type
+
         return {
           slug,
           content,
@@ -33,30 +32,31 @@ export async function getBlogs(): Promise<Blog[]> {
         } as Blog;
       })
   );
-  // sort by dates
-  return allBlogsData.sort((a: Blog, b: Blog) => (a.date < b.date ? 1 : -1));
-}
+
+  return allBlogsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+});
 
 // get blog
-export async function getBlog(slug: string): Promise<Blog | undefined> {
-  try {
-    // get by slug i.e filname
-    const fullPath = path.join(blogsDirectory, `${slug}.mdx`);
-    // get file content
-    const fileContents = await fs.readFile(fullPath, "utf8");
-    const { data, content } = matter(fileContents);
-    // return as Blog type
-    return {
-      slug,
-      content,
-      title: data.title,
-      description: data.description || "",
-      date: data.date,
-      tags: data.tags || [],
-      image: data.image || "",
-    } as Blog;
-  } catch (err: unknown) {
-    console.error(err);
-    return undefined;
+export const getBlog = cache(
+  async (slug: string): Promise<Blog | undefined> => {
+    try {
+      const fullPath = path.join(blogsDirectory, `${slug}.mdx`);
+      const fileContents = await fs.readFile(fullPath, "utf8");
+
+      const { data, content } = matter(fileContents);
+
+      return {
+        slug,
+        content,
+        title: data.title,
+        description: data.description || "",
+        date: data.date,
+        tags: data.tags || [],
+        image: data.image || "",
+      } as Blog;
+    } catch (err) {
+      console.error(err);
+      return undefined;
+    }
   }
-}
+);
